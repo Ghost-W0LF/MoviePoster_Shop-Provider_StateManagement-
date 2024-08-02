e6783a580dfc88f5f08109f23280a555cddaea82
Abhinab Khatri <abhinabkhatri@GhostWolf.local>
2024-08-02 16:41:06 +0545

    changed model

--- lib/model.dart
+++ lib/model.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:stateManagement/screens/cart.dart';
 
 class MovieProvider {
   final String title;
@@ -85,61 +84,8 @@ class Movie extends ChangeNotifier {
 
   @override
   void notifyListeners() {
-    // TODO: implement notifyListeners
+
     super.notifyListeners();
   }
 }
 
-
-/*
-class Movie extends ChangeNotifier {
-  Movie(this.title);
-  final String title;
-
- 
-/*   List<String> movies = [
-    'The Shawshank Redemption',
-    'The Godfather',
-    'The Dark Knight',
-    'Pulp Fiction',
-    'Forrest Gump',
-    'Inception',
-    'Fight Club',
-    'The Matrix',
-    'Goodfellas',
-    'The Lord of the Rings: The Fellowship of the Ring',
-    'Star Wars: Episode V - The Empire Strikes Back',
-    'The Silence of the Lambs',
-    'Saving Private Ryan',
-    "Schindler's List",
-    'Interstellar',
-  ]; */
-
-  List<int> wishList = [];
-
-  void add(index) {
-    wishList.add(index);
-    notifyListeners();
-  }
-
-  void remove(index) {
-    wishList.remove(index);
-    notifyListeners();
-  }
-
-  @override
-  void notifyListeners() {
-    // TODO: implement notifyListeners
-    super.notifyListeners();
-  }
-  List<Movie> movies = [
-    Movie(
-      title: 'The Shawshank Redemption',
-      director: 'Frank Darabont',
-      releaseYear: 1994,
-      genres: ['Drama'],
-    ),
-  ];
-}
- 
- */
